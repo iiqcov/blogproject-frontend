2b886463fe628fb8ce4990c3c5c9570fe0f1666b
iiqcov <iiqcov@gmail.com>
2024-01-19 22:52:51 +0900

    feat : change way to expression of thumbnail

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import Header from '../common/Header';
 import Sidebar from '../common/Sidebar';
 import Footer from '../common/Footer';
 
 const ArticleList = ({ url }) => {
     const [articles, setArticles] = useState([]);
-    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(url)
@@ -30,7 +29,11 @@ const ArticleList = ({ url }) => {
                     <div key={`article-${index}`}>
                         <h5>{article.title}</h5>
                         {article.folderName && <h5>{article.folderName}</h5>}
-                        {article.thumbnailLink && <img src={article.thumbnailLink} alt="thumbnail" style={{width: '500px', height: '300px'}} />}
+                        {article.thumbnailLink && (
+                            <div style={{width: '500px', height: '300px', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                <img src={article.thumbnailLink} alt="thumbnail" style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+                            </div>
+                        )}
                         <Link to={`/article/${article.id}`}>보러가기</Link>
                     </div>
                     )}
